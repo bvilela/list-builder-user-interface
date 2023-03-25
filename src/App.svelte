56fd7b6fcf3a43cb8c764a0d3623ec74838c54c2
Bruno Vilela <brunovilela2008@hotmail.com>
2023-03-25 14:40:25 -0300

    dev project

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
-    import Config from "./Config.svelte";
+    // import Config from "./Config.svelte";
+
+    import Home from "./Home.svelte";
+
 </script>
 
 <main>
-	<Config title="List Builder Configurações"/>
+	<!-- <Config title="List Builder Configurações"/> -->
+    <Home/>
 </main>
 
 <style>
---
+++ src/Home.svelte
@@ -0,0 +1,59 @@
+<script>
+    import Grid from "svelte-grid-responsive";
+    import Card, {
+        Content,
+        PrimaryAction,
+        Media,
+        MediaContent,
+    } from "@smui/card";
+</script>
+
+<main>
+    <Grid container gutter={12}>
+        <Grid md={12}>
+            <h3>Escolha a lista desejada:</h3>
+        </Grid>
+        <Grid md={4}>
+            <div class="card-display">
+                <div class="card-container">
+                    <Card>
+                        <Media class="card-media-16x9" aspectRatio="16x9">
+                            <MediaContent>
+                                <h2 class="mdc-typography--headline6 title-center">
+                                    Assistência
+                                </h2>
+                            </MediaContent>
+                        </Media>
+                        <!-- <Content style="color: #888;"
+                            >Here's some gray text down here.</Content
+                        > -->
+                    </Card>
+                </div>
+            </div></Grid
+        >
+        <Grid md={4}>
+            <div>Designações</div>
+        </Grid>
+        <Grid md={4}>
+            <div>Limpeza</div>
+        </Grid>
+        <Grid md={4}>
+            <div>Nossa Vida e Ministério</div>
+        </Grid>
+        <Grid md={4}>
+            <div>Oradores</div>
+        </Grid>
+    </Grid>
+</main>
+
+<style>
+    .title-center {
+        color: #000;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+</style>
